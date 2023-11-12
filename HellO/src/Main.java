d6831de37b652f7a64bc856ac46167cad801167b
x3mer333 <x3mer333@gamil.com>
2023-11-12 17:58:44 +0700

    Error

--- HellO/src/Main.java
+++ HellO/src/Main.java
@@ -2,9 +2,7 @@ import java.util.logging.Logger;
 
 public class Main {
 
-    private static final Logger logger = Logger.getLogger(Main.class.getName());
-
     public static void main(String[] args) {
-        logger.info("Hello world!");
+        System.out.println("Hello World");
     }
 }
